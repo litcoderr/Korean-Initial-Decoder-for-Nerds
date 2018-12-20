a9a90225a0cfdc50e548f93507b7479fb6ab81e1
litcoderr <litcoderr@gmail.com>
2018-12-20 16:21:15 +0900

    updated parsing function

--- Model/convertData.py
+++ Model/convertData.py
@@ -1,6 +1,9 @@
 import os
+from xml.etree import ElementTree as ET
+from bs4 import BeautifulSoup as BS
 
-raw_data_path = "./Data/kowiki-latest-pages-articles.xml"
+#raw_data_path = "./Data/kowiki-latest-pages-articles.xml"
+raw_data_path = "./Data/small_sample.xml"
 converted_data_path = "./Data/converted.txt"
 
 ## Setup
@@ -11,7 +14,14 @@ assert os.path.isfile(raw_data_path), "No Raw Data Found"
 if not os.path.isfile(converted_data_path):
     converted_file = open(converted_data_path,"w+")
 
+print("Start Parsing")
+xmlTree = ET.parse(raw_data_path)
+xmlRoot = xmlTree.getroot()
+xmlString = ET.tostring(xmlRoot,encoding='utf8',method='xml')
+soup = BS(xmlString)
+tag = soup.find_all("text")
 
+for idx, element in enumerate(tag):
+    print("{} : {}".format(idx,element.text))
 
-# End Program
-converted_file.close()
+print("Finished Parsing")
