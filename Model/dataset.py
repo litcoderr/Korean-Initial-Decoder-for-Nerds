4936aac1d8e153d8e9cb1e4168d6fd21b7528b73
litcoderr <litcoderr@gmail.com>
2018-12-24 14:34:24 +0900

    made length retrieval function

--- Model/dataset.py
+++ Model/dataset.py
@@ -1,28 +1,38 @@
 import torch
 from torch.utils import data
 import pandas as pd
+import json
 
 class Dataset(data.Dataset):
     def __init__(self):
         self.original_header = "./Data/original"
         self.initial_header = "./Data/initial"
+        self.config_data_path = "./Data/config.json"
 
         self.dataset_num = 1
         self.data_num = 1
 
-        self.original_file = pd.read_csv(self.generate_file_name(self.original_header),"a")
-        self.initial_file = pd.read_csv(self.generate_file_name(self.initial_header),"a")
+        config_file = open(self.config_data_path, "r")
+        self.config_data = json.load(config_file)
+
+        self.original_file = pd.read_csv(self.generate_file_name(self.original_header),header=None)
+        self.original_file.columns = ['letter']
+        self.initial_file = pd.read_csv(self.generate_file_name(self.initial_header),header=None)
+        self.original_file.columns = ['letter']
 
     def __len__(self):
-        pass
+        length = 0
+        for i in range(1,int(self.config_data["Dataset_Num"])+1):
+            length += int(self.config_data["dataset-{}".format(i)])
+        return length
 
     def __getitem__(self, index):
-        arr = [1,2,3,4,5]
-        return arr[index]
+        #TODO create one data based on index --> one_hot_encode --> return
+        pass
 
     def generate_file_name(self,header):
         return "{}-{}.csv".format(header,self.dataset_num)
 
 if __name__ == "__main__":
     data = Dataset()
-    print(data[0])
+    print(len(data))
