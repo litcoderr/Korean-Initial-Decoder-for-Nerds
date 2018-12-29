6caf3f0337361c7865ff8a1d249b281ebf2799ba
litcoderr <litcoderr@gmail.com>
2018-12-30 08:49:40 +0900

    added get_item function to dataset

--- Model/dataset.py
+++ Model/dataset.py
@@ -24,11 +24,36 @@ class Dataset(data.Dataset):
         return length
 
     def __getitem__(self, index):
-        #TODO create one data based on index --> one_hot_encode --> return
+        # return file-index and index in that file
+        which_file,which_index = self.which_file(index)
+        # if file-index is different change if not do nothing
+        if self.current_dataset != which_file:
+            self.current_dataset = which_file
+            self.original_file = self.load_file(self.original_header)
+        # get letter of index
+        character = self.original_file.at[which_index,"letter"]
+        print(character)
+        # one hot encode
+        letter = self.letter_to_onehot(character)
+        # return
+        return letter
         pass
 
+    def which_file(self,index):
+        temp_file = 1
+        temp_index = index
+
+        for i in range(1,int(self.config_data["Dataset_Num"])+1):
+            temp_index = temp_index - int(self.config_data["dataset-{}".format(i)])
+            if temp_index < 0:
+                temp_index = temp_index + int(self.config_data["dataset-{}".format(i)])
+                temp_file = i
+                break
+        return (temp_file,temp_index)
+
+
     def generate_file_name(self,header):
-        return "{}-{}.csv".format(header,self.dataset_num)
+        return "{}-{}.csv".format(header,self.current_dataset)
 
     def load_file(self,header):
         temp = pd.read_csv(self.generate_file_name(header),header=None)
@@ -47,6 +72,8 @@ class Dataset(data.Dataset):
         return temp
 
     def korean_to_index(self,letter):
+        if letter == '':
+            letter = "N"
         index = self.all_korean_characters.index(letter)
         return index
 
@@ -56,6 +83,14 @@ class Dataset(data.Dataset):
         vector[index] = 1
         return vector
 
+    def letter_to_onehot(self,letter):
+        letter = hgtk.letter.decompose(letter)
+        result = [[0] for _ in range(3)]
+        for idx, char in enumerate(letter):
+            result[idx] = self.korean_to_onehot(char)
+        return result
+
 if __name__ == "__main__":
     data = Dataset()
     print(data.original_file)
+    print(data.__getitem__(5000000))
