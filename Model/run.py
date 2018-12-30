1d588d2c61c174358fa854992fecc301deee242c
litcoderr <litcoderr@gmail.com>
2018-12-30 22:46:53 +0900

    made testing code

--- Model/run.py
+++ Model/run.py
@@ -4,47 +4,49 @@ import torch
 import os
 import hgtk
 
-dir_path = os.path.dirname(os.path.realpath(__file__))+"/trained/model.ckpt"
+class Runner():
+    def __init__(self):
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))+"/trained/model.ckpt"
 
-model = InitialPrediction()
-model.load_state_dict(torch.load(dir_path))
+        self.model = InitialPrediction()
+        self.model.load_state_dict(torch.load(self.dir_path))
 
-dataset = Dataset()
+        self.dataset = Dataset()
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-embed_size = 26
-hidden_size = 26
-num_classes=53
-batch_size=1
+        self.embed_size = 26
+        self.hidden_size = 26
+        self.num_classes=53
+        self.batch_size=1
 
-def extract_chosung(query):
-    chosung = []
-    for letter in query:
-        raw = hgtk.letter.decompose(letter)[0]
-        one_hot = dataset.korean_to_onehot(raw)
-        chosung.append([raw,one_hot])
-    return chosung
+    def extract_chosung(self,query):
+        chosung = []
+        for letter in query:
+            raw = hgtk.letter.decompose(letter)[0]
+            one_hot = self.dataset.korean_to_onehot(raw)
+            chosung.append([raw,one_hot])
+        return chosung
 
+    def run(self):
+        while(True):
+            query = input("Insert Query : ")
+            if query=="exist":
+                break
+            print("Query : {}".format(query))
+            constructed = []
+            # Extract cho sung
+            chosung = self.extract_chosung(query)
+            # For each word -> Feed Forward
+            hx = torch.zeros(self.batch_size, self.hidden_size).to(self.device)
+            cx = torch.zeros(self.batch_size, self.hidden_size).to(self.device)
+            for letter in chosung:
+                constructed.append(letter[0])
+                x = torch.FloatTensor(letter[1]).unsqueeze(0)
+                for seq in range(3):
+                    x,hx,cx = self.model(x,hx,cx)
+                    if seq<2:
+                        constructed.append(self.dataset.onehot_to_korean(x.view(-1)))
+            print(constructed)
 
-while(True):
-    query = input("Insert Query : ")
-    if query=="exist":
-        break
-    print("Query : {}".format(query))
-    constructed = []
-    # Extract cho sung
-    chosung = extract_chosung(query)
-    # For each word -> Feed Forward
-    hx = torch.zeros(batch_size, hidden_size).to(device)
-    cx = torch.zeros(batch_size, hidden_size).to(device)
-    for letter in chosung:
-        constructed.append(letter[0])
-        x = torch.FloatTensor(letter[1]).unsqueeze(0)
-        for seq in range(3):
-            x,hx,cx = model(x,hx,cx)
-            if seq<2:
-                constructed.append(dataset.onehot_to_korean(x.view(-1)))
-    print(constructed)
-
-print("Run Finished")
+        print("Run Finished")
--- test.py
+++ test.py
@@ -1,4 +1,18 @@
 from Model.train import Trainer
+from Model.run import Runner
+import argparse
+
+# Parse Mode
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", type=str, default="run", required=True, help="Mode: 1) train 2) run")
+args = parser.parse_args()
 
 trainer = Trainer()
-trainer.train()
+runner = Runner()
+
+if args.mode == "train":
+    trainer.train()
+elif args.mode == "run":
+    runner.run()
+else:
+    print("Please enter valid mode")
